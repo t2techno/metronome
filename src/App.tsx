fa61369692cd2ac3d6a982a3310ca2f84ae4eebf
t2techno <ttuschhoff11@gmail.com>
2024-10-27 14:59:46 -0500

    move react router to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { PropsWithChildren, useMemo } from "react";
+
 import styles from "./app.module.css";
 import useTimer from "./hooks/useTimer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import useSound from "use-sound";
 import MetronomePage from "./pages/metronome/MetronomePage";
+import TabLayout from "./pages/tab-layout";
+import MusicPage from "./pages/music";
 
-function App() {
+const App: React.FC<PropsWithChildren> = ({ children }) => {
   const [play, { stop }] = useSound("/sounds/cowbellSprite.mp3", {
     sprite: {
       full: [0, 313],
@@ -14,9 +19,16 @@ function App() {
 
   return (
     <main className={styles.wrapper}>
-      <MetronomePage playSound={play}/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<TabLayout />}>
+            <Route index element={<MetronomePage playSound={play} />} />
+            <Route path="/music" element={<MusicPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </main>
   );
-}
+};
 
 export default App;
