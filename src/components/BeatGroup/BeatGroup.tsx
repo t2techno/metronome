bd7a7e71c801c4376c8a7c942164bda05d234644
t2techno <ttuschhoff11@gmail.com>
2024-10-28 20:40:09 -0500

    filling in music page a little

--- src/components/BeatGroup/BeatGroup.tsx
+++ src/components/BeatGroup/BeatGroup.tsx
@@ -1,11 +1,45 @@
+import Field from "../BeatGroupField/BeatGroupField";
+import TimeSignature from "../TimeSignature";
 import styles from "./beat-group.module.css";
-const BeatGroup = () => {
+
+export interface iBeatGroup {
+  key: number;
+  start: number;
+  end: number;
+  tempo: number;
+  beatsPerMeasure: number;
+  subdivision: number;
+}
+
+export type GroupFields =
+  | "start"
+  | "end"
+  | "tempo"
+  | "beatsPerMeasure"
+  | "subdivision";
+
+interface iBeatProps {
+  currentMeasure: number;
+  group: iBeatGroup;
+  updateGroup: (newGroup: iBeatGroup) => void;
+}
+const BeatGroup: React.FC<iBeatProps> = ({
+  group,
+  currentMeasure,
+  updateGroup,
+}) => {
   return (
     <div className={styles.wrapper}>
-      <p>Time Signature</p>
-      <p>Tempo</p>
-      <p>Current Measure</p>
-      <p>Current Beat</p>
+      <Field label="Start">{group.start}</Field>
+      <Field label="End">{group.end >= 0 ? group.end : "-"}</Field>
+      <Field label="Current">{currentMeasure}</Field>
+      <Field label="TimeSignature">
+        <TimeSignature
+          beatsPerMeasure={group.beatsPerMeasure}
+          subdivision={group.subdivision}
+        />
+      </Field>
+      <Field label="Tempo">{group.tempo}</Field>
     </div>
   );
 };
--- src/pages/music/MusicPage.tsx
+++ src/pages/music/MusicPage.tsx
@@ -1,23 +1,64 @@
 import { useState } from "react";
-import BeatGroup from "../../components/BeatGroup";
+import BeatGroup, { iBeatGroup } from "../../components/BeatGroup";
 import styles from "./music-page.module.css";
+import TimeSignature from "../../components/TimeSignature";
 
-interface iBeatGroups {
-  start: number;
-  length: number;
-  tempo: number;
-  beatsPerMeasure: number;
-  subdivision: number;
-}
+const testGroup: iBeatGroup = {
+  key: Math.random(),
+  start: 1,
+  end: 25,
+  tempo: 60,
+  beatsPerMeasure: 4,
+  subdivision: 4,
+};
 
 const MusicPage = () => {
-  const [beatGroups, setBeatGroups] = useState<Array<iBeatGroups>>([]);
+  const [beatGroups, setBeatGroups] = useState<Array<iBeatGroup>>([testGroup]);
+  const [currentGroup, setCurrentGroup] = useState(0);
+  const [currentMeasure, setCurrentMeasure] = useState(0);
+
+  const handleGroupUpdate = (idx: number, newGroup: iBeatGroup) => {
+    const newGroups = [...beatGroups];
+    newGroups[idx] = newGroup;
+    setBeatGroups(newGroups);
+  };
+
+  const createNewGroup = () => {
+    const groups = [...beatGroups];
+    const lastGroup = groups[groups.length - 1];
+    const newGroup: iBeatGroup = {
+      ...lastGroup,
+      key: Math.random(),
+      start: lastGroup.end + 1,
+      end: -1,
+    };
+    groups.push(newGroup);
+    setBeatGroups(groups);
+  };
+
   return (
     <div className={styles.wrapper}>
-      <p>Time Signature</p>
-      <p>Beat Indicators...</p>
+      {beatGroups.length > 0 && (
+        <>
+          <TimeSignature
+            beatsPerMeasure={beatGroups[currentGroup].beatsPerMeasure}
+            subdivision={beatGroups[currentGroup].subdivision}
+          />
+          {<p>beat indicators...</p>}
+        </>
+      )}
       <hr />
-      <BeatGroup />
+      {beatGroups.map((group, idx) => (
+        <BeatGroup
+          key={group.key}
+          currentMeasure={currentMeasure}
+          group={group}
+          updateGroup={(newGroup: iBeatGroup) => {
+            handleGroupUpdate(idx, newGroup);
+          }}
+        />
+      ))}
+      <button onClick={createNewGroup}>Add group</button>
     </div>
   );
 };
