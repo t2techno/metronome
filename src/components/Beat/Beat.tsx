ed4d8d8774b4c4442b175a85723d5173fdacffc8
t2techno <ttuschhoff11@gmail.com>
2024-11-22 14:01:34 -0600

    beat indicator adjust

--- src/components/Beat/Beat.tsx
+++ src/components/Beat/Beat.tsx
@@ -1,16 +1,21 @@
 import styles from "./beat.module.css";
 
 interface iBeatProps {
-  active: boolean;
+  active?: boolean;
+  downBeat?: boolean;
 }
 
-const Beat: React.FC<iBeatProps> = ({ active }) => {
+const Beat: React.FC<iBeatProps> = ({ active, downBeat = false }) => {
+  let size = downBeat ? 30 : 25;
+  if (active) {
+    size += size * 0.25;
+  }
   return (
     <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
       <circle
         cx="50"
         cy="50"
-        r="50"
+        r={size}
         className={`${styles.beat} ${active && styles.active}`}
       />
     </svg>
